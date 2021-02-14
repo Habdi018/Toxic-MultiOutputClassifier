35beee2c319bff2f1b29b0a91ea9145e1529033b
Habdi018 <habdi.cnlp@gmail.com>
2021-02-14 02:37:33 -0500

    Update NLP.py

--- NLP.py
+++ NLP.py
@@ -8,30 +8,28 @@ from transformers import BertTokenizer
 stopwords = [word for word in open("stopwords.txt").read().split("\n")]
 
 # preprocessing
-def preprocess(comment, t, number_of_features=None):
-    # comment = comment.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
-    if t == "nltk":
-        comment = nltk.tokenize.word_tokenize(str(comment.lower())) # lower and tokenize
-    if t == "spacy":
-        nlp = spacy.load("en_core_web_sm")
-        doc = nlp(str(comment.lower()))
-        comment = [token.text for token in doc]
-    if t == "keras":
-        tok = tf.keras.preprocessing.text.Tokenizer(num_words=number_of_features,
-                                                    # filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
-                                                    lower=True, split=' ', char_level=False, oov_token=None,
-                                                    document_count=0)
-        comment = tok.fit_on_texts(comment)
-    if t == "transformers":
-        tok = BertTokenizer.from_pretrained("bert-base-uncased")
-        comment = tok.tokenize(comment)
-    # comment = [word for word in comment if word not in stopwords]  # remove stopwords
+def tok_nltk(comment):
+    comment = nltk.tokenize.word_tokenize(comment)  # tokenize
+    # comment = nltk.tokenize.word_tokenize(str(comment.lower()))  # lower and tokenize
+    # comment = [nltk.stem.WordNetLemmatizer().lemmatize(w) for w in comment]  # lemmatize
+    return comment
+
+def tok_spacy(comment):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(str(comment.lower()))
+    comment = [token.text for token in doc]
+    # comment = [nltk.stem.WordNetLemmatizer().lemmatize(w) for w in comment]  # lemmatize
+    return comment
+
+def tok_transformers(comment):
+    tok = BertTokenizer.from_pretrained("bert-base-uncased")
+    comment = tok.tokenize(comment)
     # comment = [nltk.stem.WordNetLemmatizer().lemmatize(w) for w in comment]  # lemmatize
     return comment
 
 def keras_preprocess(train, val, number_of_features=None):
     # tokenization
-    max_seq_length = (train["comment_text"].apply(lambda x: len(preprocess(x, t="nltk")))).max()
+    max_seq_length = (train["comment_text"].apply(lambda x: len(tok_nltk(x)))).max()
     print("max_seq_length is", max_seq_length)
 
     tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=number_of_features,
@@ -48,7 +46,8 @@ def keras_preprocess(train, val, number_of_features=None):
     validation_tokenized = tf.keras.preprocessing.sequence.pad_sequences(validation_sequences, maxlen=max_seq_length)
     return train_tokenized, validation_tokenized, number_of_features, max_seq_length
 
-# comment = "Stupid               Damn it! i was mking a new page so i will potentially violate! To:Wikipedia"
-# print(preprocess(comment, t="spacy", number_of_features=None))
-# print(preprocess(comment, t="nltk", number_of_features=None))
-# print(preprocess(comment, t="transformers", number_of_features=None))
+# if __name__ == "__main__":
+    # comment = "Stupid               Damn it! i was mking a new page so i will potentially violate! To:Wikipedia"
+    # print(tok_nltk(comment))
+    # print(tok_spacy(comment))
+    # print(tok_transformers(comment))
